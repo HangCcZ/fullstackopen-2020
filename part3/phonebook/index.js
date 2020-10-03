21b1173c90868bf67ded637679aa9dbd059fb360
HangCcZ <hangchen1128@gmail.com>
2020-10-02 22:55:49 -0400

    test

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,7 +1,17 @@
 const express = require('express');
+const morgan = require('morgan');
 const app = express();
+const cors = require('cors');
+app.use(cors());
+
+morgan.token('data', function getData(req) {
+  return JSON.stringify(req.body);
+});
 
 app.use(express.json());
+app.use(
+  morgan(':method :url :status :res[content-length] - :response-time ms :data')
+);
 
 let persons = [
   {
@@ -111,7 +121,7 @@ app.post('/api/persons', (req, res) => {
   res.json(persons);
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
