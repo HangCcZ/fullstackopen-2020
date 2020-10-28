f0c8169e6aaf40372ba2de6f0b2f0cf411539711
HangCcZ <hangchen1128@gmail.com>
2020-10-28 18:33:28 -0400

    added 6.3.-6.5.

--- part6/redux-anecdotes/src/App.js
+++ part6/redux-anecdotes/src/App.js
@@ -1,35 +1,58 @@
-import React from 'react'
-import { useSelector, useDispatch } from 'react-redux'
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
 
 const App = () => {
-  const anecdotes = useSelector(state => state)
-  const dispatch = useDispatch()
+  const anecdotes = useSelector((state) => state);
+  const dispatch = useDispatch();
+
+  const updateVote = (id) => {
+    return {
+      type: "VOTE",
+      data: { id },
+    };
+  };
 
   const vote = (id) => {
-    console.log('vote', id)
-  }
+    dispatch(updateVote(id));
+  };
+
+  const createAnecdote = (content) => {
+    return {
+      type: "NEW",
+      data: { content },
+    };
+  };
+
+  const addAnecdote = (event) => {
+    event.preventDefault();
+    const content = event.target.newAnecdote.value;
+    event.target.newAnecdote.value = "";
+    dispatch(createAnecdote(content));
+  };
 
   return (
     <div>
       <h2>Anecdotes</h2>
-      {anecdotes.map(anecdote =>
-        <div key={anecdote.id}>
-          <div>
-            {anecdote.content}
+      {anecdotes
+        .sort((a, b) => b.votes - a.votes)
+        .map((anecdote) => (
+          <div key={anecdote.id}>
+            <div>{anecdote.content}</div>
+            <div>
+              has {anecdote.votes}
+              <button onClick={() => vote(anecdote.id)}>vote</button>
+            </div>
           </div>
-          <div>
-            has {anecdote.votes}
-            <button onClick={() => vote(anecdote.id)}>vote</button>
-          </div>
-        </div>
-      )}
+        ))}
       <h2>create new</h2>
-      <form>
-        <div><input /></div>
-        <button>create</button>
+      <form onSubmit={addAnecdote}>
+        <div>
+          <input name="newAnecdote" />
+        </div>
+        <button type="submit">create</button>
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -5,35 +5,40 @@ const anecdotesAtStart = [
   "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
   "Premature optimization is the root of all evil.",
   "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
-]
+];
 
-const getId = () => (100000 * Math.random()).toFixed(0)
+const getId = () => (100000 * Math.random()).toFixed(0);
 
 const asObject = (anecdote) => {
   return {
     content: anecdote,
     id: getId(),
     votes: 0,
-  }
-}
+  };
+};
 
-const initialState = anecdotesAtStart.map(asObject)
+const initialState = anecdotesAtStart.map(asObject);
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case "VOTE":
-      const id = action.data.id
-      const anecdoteToChange = state.find((a) => a.id === id)
+      const id = action.data.id;
+      const anecdoteToChange = state.find((a) => a.id === id);
       const changedAnecdote = {
         ...anecdoteToChange,
         votes: anecdoteToChange.votes + 1,
-      }
+      };
       return state.map((anecdote) =>
         anecdote.id !== id ? anecdote : changedAnecdote
-      )
-    default:
-      return state
-  }
-}
+      );
+    case "NEW":
+      const newId = getId();
+      const anecdote = { ...action.data, id: newId, votes: 0 };
+      return [...state, anecdote];
 
-export default reducer
+    default:
+      return state;
+  }
+};
+
+export default reducer;
