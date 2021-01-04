70c9ee54fa723078c41ad64eef534b6bf154b909
HangCcZ <hangchen1128@gmail.com>
2021-01-04 00:00:36 -0500

    added 9.6-9.7

--- part9/exerciseCalculator.ts
+++ part9/exerciseCalculator.ts
@@ -9,33 +9,33 @@ interface Evaluation
     average: number
 }
 
-interface MultiplyValues{
-    target:number,
-    hourPerDay:Array<number>
-}
+// interface MultiplyValues{
+//     target:number,
+//     hourPerDay:Array<number>
+// }
 
 const calculateExercises = (hourPerDay:Array<number>,target:number):Evaluation=>{
     const periodLength = hourPerDay.length;
     const validDays = hourPerDay.filter(hour=>hour>0);
     const trainingDays = validDays.length;
     const average = validDays.reduce((accumulator,currentValue)=>accumulator+currentValue) / periodLength
-    const hourDiff = target - average
-    let rating, ratingDescription, success
+    const hourDiff = target - average;
+    let rating, ratingDescription, success;
 
     if(hourDiff>2){
-        success = false
-        rating = 1
-        ratingDescription = "Try your best next time"
+        success = false;
+        rating = 1;
+        ratingDescription = "Try your best next time";
     }
     else if(hourDiff>0){
-        success = false
-        rating = 2
-        ratingDescription = 'Not too bad but could be better'
+        success = false;
+        rating = 2;
+        ratingDescription = 'Not too bad but could be better';
     }
     else{
-        success = true
-        rating = 3
-        ratingDescription = "Awesome! Keep it up!"
+        success = true;
+        rating = 3;
+        ratingDescription = "Awesome! Keep it up!";
     }
 
     return {
@@ -46,19 +46,20 @@ const calculateExercises = (hourPerDay:Array<number>,target:number):Evaluation=>
         ratingDescription,
         target,
         average
-    }
-}
+    };
+};
 
-const parseArguments = (args:Array<string>):MultiplyValues=>{
-    let hourPerDay = []
-    for(const arg of args.slice(3)){
-        hourPerDay.push(Number(arg))
-    }
+// const parseArguments = (args:Array<string>):MultiplyValues=>{
+//     // eslint-disable-next-line prefer-const
+//     let hourPerDay = [];
+
+//     for(const arg of args.slice(3)){
+//         hourPerDay.push(Number(arg));
+//     }
     
-    return {
-        hourPerDay,target:Number(args[2])
-    }
-}
+//     return {
+//         hourPerDay,target:Number(args[2])
+//     };
+// };
 
-const {hourPerDay,target} = parseArguments(process.argv)
-console.log(calculateExercises(hourPerDay,target))
+export {calculateExercises};
--- part9/index.ts
+++ part9/index.ts
@@ -1,16 +1,19 @@
-import express from 'express'
-import {calculateBmi} from './calculateBmi'
-const app = express()
+import express,{Request,Response} from 'express';
+import bodyParser from 'body-parser';
+import {calculateBmi} from './calculateBmi';
+import {calculateExercises} from './exerciseCalculator'
+const app = express();
+app.use(bodyParser.json());
 
 app.get('/',(_req,res)=>{
-    res.send("hello")
-})
+    res.send("hello");
+});
 
 app.get('/bmi',(req,res)=>{
-    const {height,weight} = req.query
+    const {height,weight} = req.query;
 
     if(!Number(height) || !Number(weight)){
-        res.send({ error: "malformatted parameters"})
+        res.send({ error: "malformatted parameters"});
     }
     else{
         const bmiResult = calculateBmi(Number(height),Number(weight))
@@ -18,13 +21,42 @@ app.get('/bmi',(req,res)=>{
             weight:Number(weight),
             height:Number(height),
             bmi:bmiResult
-        }
-        res.send(result)
+        };
+        res.send(result);
     }
    
-})
+});
 
-const PORT = 3000
+app.post('/exercise',(req:Request,res:Response)=>{
+    
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const hourPerDay:any = req.body.daily_exercises;
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const target:any = req.body.target;
+    
+
+    if(!hourPerDay || !target){
+        console.log('some error');
+        res.send({
+            error: "parameters missing"
+          }
+          );
+    }
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+   else if(!Number(target) || !Array.isArray(hourPerDay) || hourPerDay.filter((val:any)=> typeof val==='string').length>0)
+   {
+    res.send({
+        error: "malformatted parameters"
+      });
+   }
+   else{
+      
+       res.send(calculateExercises(hourPerDay,Number(target)));
+   }
+   
+});
+
+const PORT = 3000;
 app.listen(PORT,()=>{
-    console.log(`Server running on port ${PORT}`)
-})
+    console.log(`Server running on port ${PORT}`);
+});
